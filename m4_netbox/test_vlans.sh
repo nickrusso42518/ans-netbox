12d4c4ed387d7e7e6b7a0ee1984ddae4a5580cd5
nick <nickrus@cisco.com>
2021-01-11 17:39:34 -0500

    circuit fix

--- m4_netbox/test_vlans.sh
+++ m4_netbox/test_vlans.sh
@@ -1,6 +1,7 @@
-#!
-# Bash script to simplify long command
-echo "Password retrieved from env var SSHPASS"
+#!/bin/bash
+# Shell script to simplify long Ansible command
+echo "Set password using the env var below"
+echo "export SSHPASS=<password>"
 sshpass -e ansible-playbook get_vlans_v3.yml \
   --inventory netbox_inv.yml \
   --extra-vars '{"strict_mode": true}' \
