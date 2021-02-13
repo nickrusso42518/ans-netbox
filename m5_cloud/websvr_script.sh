b8548f983c7372035c5a3ab7f8dd5da6f128f585
nick <njrusmc@gmail.com>
2021-02-13 10:17:10 -0500

    m5 complete

--- m5_cloud/websvr_script.sh
+++ m5_cloud/websvr_script.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-# Create route to Globomantics network via PAN FW inside interface
-# Assumes server has 2 interfaces:
-#  eth0: public mgmt with dynamic EIP (doesn't matter)
-#  eth1: Globo Servers subnet with IP 10.0.2.78
-ip route add 192.168.0.0/16 via 10.0.2.77 dev eth1
-
-# Install and start Apache web server
-yum install httpd -y
-systemctl start httpd
-
-# Create a trivial file that can be downloaded as a quick test
-echo "Globo test file" >> /var/www/html/test.txt
-
-# LEGACY -- delete
-#echo "192.168.0.0/16 via 10.0.2.77" >> /etc/sysconfig/network-scripts/route-eth0
-#systemctl restart network
+# Image ID: ami-0fa59001d65100e7f (Apache on Centos 7.6)
+# Local eth0 IP: 10.0.2.78 in server subnet
+# Create route to Globomantics network via PAN FW server interface
+ip route add 192.168.0.0/16 via 10.0.2.77 dev eth0
